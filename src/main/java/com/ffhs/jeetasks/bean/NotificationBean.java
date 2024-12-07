3f5a3ab271f8165f0cd6d4aa8e2f40f62e783fcc
Julian Luginbühl <julian.luginbuehl@informaticon.com>
2024-12-07 12:45:27 +0100

    chore: NotificationBean refactoring

--- src/main/java/com/ffhs/jeetasks/bean/NotificationBean.java
+++ src/main/java/com/ffhs/jeetasks/bean/NotificationBean.java
@@ -5,33 +5,53 @@ import com.ffhs.jeetasks.service.NotificationService;
 import jakarta.enterprise.context.SessionScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
 import java.util.List;
 
-@Data
+/**
+ * Bean responsible for managing user notifications.
+ */
 @Named
 @SessionScoped
 public class NotificationBean implements Serializable {
 
     @Inject
     private NotificationService notificationService;
-    @Inject
-    private LoginBean loginBean;
 
     private List<Notification> openNotifications;
 
+    /**
+     * Retrieves all open (non-dismissed) notifications for the logged-in user.
+     *
+     * @return List of open notifications.
+     */
     public List<Notification> getOpenNotifications() {
-        this.openNotifications = notificationService.findAllNotificationsForUserNotDismissed();
+        if (openNotifications == null) {
+            loadOpenNotifications();
+        }
         return openNotifications;
     }
 
+    /**
+     * Loads open (non-dismissed) notifications for the logged-in user into the bean.
+     */
     public void loadOpenNotifications() {
         this.openNotifications = notificationService.findAllNotificationsForUserNotDismissed();
     }
 
+    /**
+     * Marks a notification as dismissed and updates the notification list.
+     *
+     * @param notification The notification to dismiss.
+     */
     public void dismissNotification(Notification notification) {
+        if (notification == null) {
+            throw new IllegalArgumentException("Notification cannot be null");
+        }
+
         notification.setDismissed(true);
         notificationService.updateModel(notification);
         loadOpenNotifications();
