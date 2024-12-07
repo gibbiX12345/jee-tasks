c6815ce8f400e7d652411abe6c97b03135071614
Julian Luginbühl <julian.luginbuehl@informaticon.com>
2024-12-07 13:36:51 +0100

    chore: extract RegistrationFormDTO

--- src/main/java/com/ffhs/jeetasks/bean/user/RegistrationBean.java
+++ src/main/java/com/ffhs/jeetasks/bean/user/RegistrationBean.java
@@ -1,5 +1,7 @@
 package com.ffhs.jeetasks.bean.user;
 
+import com.ffhs.jeetasks.dto.LoginFormDTO;
+import com.ffhs.jeetasks.dto.RegistrationFormDTO;
 import com.ffhs.jeetasks.entity.User;
 import com.ffhs.jeetasks.service.UserService;
 import jakarta.enterprise.context.RequestScoped;
@@ -8,6 +10,8 @@ import jakarta.inject.Named;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 
 /**
@@ -21,18 +25,9 @@ public class RegistrationBean {
     @Inject
     private UserService userService;
 
-    /**
-     * User email for registration. Must be non-empty and in a valid email format.
-     */
-    @NotEmpty(message = "Email is required")
-    @Email(message = "Invalid email address")
-    private String email;
-
-    /**
-     * User password for registration. Must be non-empty.
-     */
-    @NotEmpty(message = "Password is required")
-    private String password;
+    @Getter
+    @Setter
+    private RegistrationFormDTO registrationForm = new RegistrationFormDTO();
 
     /**
      * Registers a new user with the provided email and password.
@@ -42,8 +37,8 @@ public class RegistrationBean {
      */
     public String registerUser() {
         User user = new User();
-        user.setEmail(email);
-        user.setPasswordHash(BCrypt.hashpw(password, BCrypt.gensalt()));
+        user.setEmail(registrationForm.getEmail());
+        user.setPasswordHash(BCrypt.hashpw(registrationForm.getPassword(), BCrypt.gensalt()));
         userService.registerUser(user);
         return "/login?faces-redirect=true";
     }
